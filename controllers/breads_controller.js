5fa0d39aeb70b452668eb409ab664455581f171f
jbucsa <justin.bucsa@gmail.com>
2023-10-22 11:13:10 -0500

    update controllers/breads_controller.js file

--- controllers/breads_controller.js
+++ controllers/breads_controller.js
@@ -2,9 +2,18 @@ const express = require('express');
 const breads = express.Router();
 const Bread = require('../models/bread,js')
 
+//INDEX
 breads.get('/', (req, res) =>
 {
     res.send(Bread);
+});
+
+
+//SHOW
+// localhost/breads/0
+breads.get('/0', (req, res) =>
+{
+    res.send(Bread[0]);
 });
 
 module.exports = breads
