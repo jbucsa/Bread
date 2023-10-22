b2fc771c7298b842ea0fa852653e81a1dda2824c
jbucsa <justin.bucsa@gmail.com>
2023-10-22 11:04:11 -0500

    Update server.js file

--- server.js
+++ server.js
@@ -1,5 +1,16 @@
-const express = require('express')
-require('dotenv').config()
+const express = require('express');
+require('dotenv').config();
 
-const PORT = process.env.PORT
-console.log(PORT)
+const PORT = process.env.PORT;
+console.log(PORT);
+
+const app = express();
+
+app.get('/', (req,res) =>
+{
+    res.send('Welcome to an Awesome Application about Breads!!!');
+});
+
+app.listen(PORT, ()=> {
+    console.log('Listening on port: ', PORT);
+});
